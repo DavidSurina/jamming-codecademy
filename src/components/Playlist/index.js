ebf3eecbdc9ba48aeb32fe04c6b5d4b49124e161
David Surina <david.surina22@gmail.com>
2021-05-05 20:33:04 +0200

    created base for playlist component

---
+++ src/components/Playlist/index.js
@@ -0,0 +1,10 @@
+import './style.css';
+
+export default function Playlist() {
+  return (
+    <div class="Playlist">
+      <input value="New Playlist" />
+      <button class="Playlist-save">SAVE TO SPOTIFY</button>
+    </div>
+  );
+}
