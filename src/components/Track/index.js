e9a561b8744d21ce6bf1a6b5d211efdcdf1eea21
David Surina <david.surina22@gmail.com>
2021-05-05 20:36:50 +0200

    created base for track component

---
+++ src/components/Track/index.js
@@ -0,0 +1,13 @@
+import './style.css';
+
+export default function Track() {
+  return (
+    <div class="Track">
+      <div class="Track-information">
+        <h3></h3>
+        <p></p>
+      </div>
+      <button class="Track-action"></button>
+    </div>
+  );
+}
