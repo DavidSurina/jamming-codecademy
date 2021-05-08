273695336f40c6820ccd29786e0e01fbd2d9081a
David Surina <david.surina22@gmail.com>
2021-05-08 17:54:47 +0200

    added hardcodedResults array with data for testing purposes and passed it to SearchResults component as prop

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
+import {useState} from "react";
+
+import SearchBar from "./components/SearchBar";
+import SearchResults from "./components/SearchResults";
+import Playlist from "./components/Playlist";
+
 import './App.css';
 
 function App() {
+  const hardCodedResults = [{name: "Numb", artist: "Linkin Park", album: "meteora", id: 1},
+                        {name: "Foreword", artist: "Linkin Park", album: "meteora", id: 2},
+                        {name: "Hit The Floor", artist: "Linkin Park", album: "meteora", id: 3},
+                        {name: "Easier to Run", artist: "Linkin Park", album: "meteora", id: 4},
+                        {name: "From the Inside", artist: "Linkin Park", album: "meteora", id: 5},];
+
+  const [results, setResults] = useState(hardCodedResults);
+
   return (
     <div>
       <h1>Ja<span className="highlight">mmm</span>ing</h1>
       <div className="App">
-      <div className="App-playlist">
+        <SearchBar />
+        <div className="App-playlist">
+          <SearchResults results={results}/>
+          <Playlist />
+        </div>
       </div>
     </div>
-  </div>
   );
 }
 
