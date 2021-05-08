4578ae5be62e05c57578569b08eddb2b4b67e964
David Surina <david.surina22@gmail.com>
2021-05-08 18:19:16 +0200

    added rendering of TrackList component and passed playlistTracks to it as a prop

--- src/components/Playlist/index.js
+++ src/components/Playlist/index.js
@@ -1,9 +1,12 @@
+import TrackList from "./TrackList";
+
 import './style.css';
 
-export default function Playlist() {
+export default function Playlist(playlistName, playlistTracks) {
   return (
     <div className="Playlist">
       <input value="New Playlist" />
+      <TrackList tracks={playlistTracks}/>
       <button className="Playlist-save">SAVE TO SPOTIFY</button>
     </div>
   );
