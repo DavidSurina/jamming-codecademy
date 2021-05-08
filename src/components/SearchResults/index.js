1c7a6a7cb0b9307260bfdb28dfe299206f038b68
David Surina <david.surina22@gmail.com>
2021-05-08 17:57:34 +0200

    rendering tracklist component in searchResults and passing the result prop further to tracklist

--- src/components/SearchResults/index.js
+++ src/components/SearchResults/index.js
@@ -1,9 +1,12 @@
+import TrackList from "./TrackList";
+
 import "./style.css";
 
-export default function SearchResults() {
+export default function SearchResults(results) {
   return (
     <div className="SearchResults">
       <h2>Results</h2>
+      <TrackList results={results}/>
     </div>
   );
 }
