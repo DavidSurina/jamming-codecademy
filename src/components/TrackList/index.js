92c728f724b87e8a7ad388607d8c764855dc80fe
David Surina <david.surina22@gmail.com>
2021-05-08 18:07:24 +0200

    created renderedResults constant, which holds result of mapping through results prop, which should render tracks passed to the component

--- src/components/TrackList/index.js
+++ src/components/TrackList/index.js
@@ -1,5 +1,11 @@
+import Track from "./Track";
+
 import './style.css';
 
-export default function TrackList() {
-  return <div className="TrackList"></div>;
+export default function TrackList(results) {
+  const renderedResults = results.map((track) => {
+    return <Track trackInfo={track}/>;
+  })
+
+  return <div className="TrackList">{renderedResults}</div>;
 }
