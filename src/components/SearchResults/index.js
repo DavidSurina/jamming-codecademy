9c1dd9d878be8ffe4e176121ee1ca044906e01fb
David Surina <david.surina22@gmail.com>
2021-05-09 18:43:44 +0200

    passed onAdd method to tracklist component an added is removal prop for correct rendering of symbol later in track component

--- src/components/SearchResults/index.js
+++ src/components/SearchResults/index.js
@@ -2,11 +2,11 @@ import TrackList from "../TrackList";
 
 import "./style.css";
 
-export default function SearchResults({results}) {
+export default function SearchResults({results, onAdd}) {
   return (
     <div className="SearchResults">
       <h2>Results</h2>
-      <TrackList tracks={results}/>
+      <TrackList tracks={results} onAdd={onAdd} isRemoval={false}/>
     </div>
   );
 }
